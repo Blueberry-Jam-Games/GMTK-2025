294967c66865c3c3a7b17cd44de7d87cc162aee4
Quinn Richardson <quinnrichardson91@gmail.com>
2025-08-02 16:13:00 -0700

    Art stuff

---
+++ Assets/Scripts/Snake/SnakeHeadDummy.cs
@@ -0,0 +1,110 @@
+using System.Collections.Generic;
+using NUnit.Framework;
+using Unity.VisualScripting;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.Animations;
+using UnityEngine.InputSystem;
+
+public class SnakeHeadDummy : MonoBehaviour
+{
+    public TorusTransform transformation;
+
+    public float rotation = 0;
+    //private float targetRotation = 0;
+
+    public float distanceBetweenSegments = 0.1f;
+
+    public List<Vector4> positions = new List<Vector4>();
+    private List<Vector3> positionsXYZ = new List<Vector3>();
+
+    public List<TorusTransform> body = new List<TorusTransform>();
+    public float turnRadiusNerf = 2f;
+    public float speedFactor = 15f;
+
+    private InputAction Forward;
+    private InputAction Turn;
+
+    private float targetRotation;
+
+    void Start()
+    {
+        body.Clear();
+        body.AddRange(transform.parent.GetComponentsInChildren<TorusTransform>(true));
+        body.RemoveAt(0);
+        Forward = InputSystem.actions.FindAction("MoveForward");
+        Turn = InputSystem.actions.FindAction("Turn");
+    }
+
+    void FixedUpdate()
+    {
+        if (Mathf.Abs((targetRotation - rotation) % 360) <= 5)
+        {
+            targetRotation = (float)Random.Range(0, 360);
+        }
+
+        if ((targetRotation - rotation) % 360 > 180)
+        {
+            rotation -= 2;
+        }
+        else if ((targetRotation - rotation) % 360 < 180)
+        {
+            rotation += 2;
+        }
+
+        transformation.Rotate(2 / turnRadiusNerf, 0.1f);
+        transformation.MoveDirection(transformation.Rotation, 2 / speedFactor);
+
+        updatePath();
+    }
+
+    private void updatePath()
+    {
+        if (transformation.hasMoved())
+        {
+            positions.Add(transformation.GetPosition());
+            positionsXYZ.Add(transformation.domain.ConvertToXYZ(transformation.GetPosition()));   
+        }
+        int queueIndex = positions.Count - 1;
+        bool waitForValue = false;
+
+        foreach (TorusTransform s in body)
+        {
+            bool success = false;
+            for (int i = queueIndex; i > 0; i--)
+            {
+                if (Vector3.Distance(positionsXYZ[i], positionsXYZ[queueIndex]) >= distanceBetweenSegments)
+                {
+                    if (!s.gameObject.activeSelf)
+                    {
+                        s.gameObject.SetActive(true);
+                    }
+                    s.SetPosition(positions[i]);
+                    queueIndex = i;
+                    success = true;
+                    break;
+                }
+            }
+
+            if (!success)
+            {
+                waitForValue = true;
+            }
+        }
+
+        if (queueIndex > 0 && !waitForValue)
+        {
+            positions.RemoveRange(0, queueIndex);
+            positionsXYZ.RemoveRange(0, queueIndex);
+        }
+    }
+
+    void OnDrawGizmos()
+    {
+        foreach (Vector3 c in positionsXYZ)
+        {
+            Gizmos.DrawSphere(c, 0.01f);
+        }
+    }
+
+}
