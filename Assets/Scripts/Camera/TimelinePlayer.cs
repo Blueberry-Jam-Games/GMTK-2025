f72ff0f05256c0dce3f7637c8fd31d169c258f28
Quinn Richardson <quinnrichardson91@gmail.com>
2025-08-02 22:51:05 -0700

    fixes

--- Assets/Scripts/Camera/TimelinePlayer.cs
+++ Assets/Scripts/Camera/TimelinePlayer.cs
@@ -5,9 +5,16 @@ public class TimelinePlayer : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     private PlayableDirector director = new PlayableDirector();
 
+    public bool startAnimation = false;
+
     void Start()
     {
         director = GetComponent<PlayableDirector>();
+        GameObject gameplayManager = GameObject.FindGameObjectWithTag("GameManager");
+        if (gameplayManager == null && startAnimation)
+        {
+            PlayTimeline();
+        }
     }
 
 
